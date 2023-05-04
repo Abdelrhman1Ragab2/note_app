7f4b948c16248269137e58afbe9d3ecdbf19b032
Abdelrhman ragab <abdelrhman1ragab2@gmail.com>
2023-05-04 23:55:10 +0200

    #3 add assets

---
+++ lib/model/document.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'document.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class DocumentAdapter extends TypeAdapter<Document> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Document read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Document(
+      filePath: fields[0] as String,
+      folderNamePath: fields[1] as String,
+      id: fields[2] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Document obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.filePath)
+      ..writeByte(1)
+      ..write(obj.folderNamePath)
+      ..writeByte(2)
+      ..write(obj.id);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DocumentAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
